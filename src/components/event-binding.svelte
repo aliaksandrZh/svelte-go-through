6b60b8b3115dfca36ecfa22c1516846008a5ebaf
aliasksandr.zhebit <aliasksandr.zhebit@gmail.com>
2022-07-16 14:43:23 +0300

    event binding

---
+++ src/components/event-binding.svelte
@@ -0,0 +1,40 @@
+<script>
+  let count = 0;
+
+  function increaseCounter() {
+    count += 1;
+  }
+</script>
+
+<h2 class="section-example--title">Event Binding</h2>
+
+<div class="section-example">
+  <div class="section-example__view">
+    <button on:click={increaseCounter}>
+      Clicked { count } { count === 1 ? 'time' : 'times' }
+    </button>
+  </div>
+
+  <div class="section-example__code">
+      <pre>
+       &lt;script&gt;
+        <span class="indent">let count = 0;</span>
+
+        <span class="indent">function increaseCounter() &#123;</span>
+        <span class="indent"><span class="indent"> count += 1;</span></span>
+        <span class="indent">&#125;</span>
+      &lt;/script&gt;
+
+      &lt;button on:click=&#123;increaseCounter&#125;&gt;
+        <span class="indent">Clicked &#123; count &#125; &#123; count === 1 ? 'time' : 'times' &#125;</span>
+      &lt;/button&gt;
+    </pre>
+  </div>
+</div>
+
+
+<style>
+  button {
+    padding: 10px 20px;
+  }
+</style>
