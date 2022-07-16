ba29203b6f041144b045ce0dd7cee6881d12eae0
aliasksandr.zhebit <aliasksandr.zhebit@gmail.com>
2022-07-16 13:34:07 +0300

    interpolation

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Interpolation from './components/interpolation.svelte';
+</script>
+
+<header>
+    <h1>Svelte go through</h1>
+</header>
+
+<main>
+
+    <section>
+      <Interpolation/>
+    </section>
+
+</main>
+
+<style>
+
+    header {
+      padding: 20px 0;
+    }
+
+    header > h1 {
+      text-align: center;
+    }
+
+    main {
+      width: 100%;
+      height: 100%;
+      background-color: aquamarine;
+    }
+
+
+</style>
---
+++ src/components/interpolation.svelte
@@ -0,0 +1,27 @@
+<h2 class="section-example--title">Interpolation</h2>
+
+<div class="section-example">
+ <div class="section-example__view">
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aspernatur cumque deleniti distinctio dolor esse
+     ipsum libero minima neque <b> { hWorld } </b>quae qui, quidem quod rem sit tenetur totam vero voluptatem.</p>
+ </div>
+
+  <div class="section-example__code">
+      <pre>
+      &lt;p&gt;
+        <span class="indent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aspernatur cumque deleniti distinctio dolor esse ipsum libero minima neque  &lt;b&gt;&#123; hWorld &#125;&lt;b&gt;quae qui, quidem quod rem sit tenetur totam vero voluptatem.
+        </span>
+      &lt;/p&gt;
+
+
+      &lt;script&gt;
+        <span class="indent">let hWorld = "Hello world";</span>
+      &lt;/script&gt;
+    </pre>
+  </div>
+</div>
+
+<script>
+  let hWorld = "Hello world";
+</script>
+
