7d55afb7682eb34b2337d947dceb23ab4cf18491
aliasksandr.zhebit <aliasksandr.zhebit@gmail.com>
2022-07-16 14:43:49 +0300

    upd main view

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,44 @@
 <script>
   import Interpolation from './components/interpolation.svelte';
+  import Styles from './components/styles.svelte';
+  import HTMLInjection from './components/html-injection.svelte';
+  import EventBinding from './components/event-binding.svelte';
 </script>
 
 <header>
-    <h1>Svelte go through</h1>
+  <h1>Svelte go through</h1>
 </header>
 
 <main>
+  <section>
+    <Interpolation/>
+  </section>
 
-    <section>
-      <Interpolation/>
-    </section>
+  <section>
+    <Styles/>
+  </section>
 
+  <section>
+    <HTMLInjection/>
+  </section>
+
+  <section>
+    <EventBinding/>
+  </section>
 </main>
 
 <style>
+  header {
+    padding: 20px 0;
+  }
 
-    header {
-      padding: 20px 0;
-    }
+  header > h1 {
+    text-align: center;
+  }
 
-    header > h1 {
-      text-align: center;
-    }
-
-    main {
-      width: 100%;
-      height: 100%;
-      background-color: aquamarine;
-    }
-
-
+  main {
+    width: 100%;
+    height: 100%;
+    background-color: aquamarine;
+  }
 </style>
