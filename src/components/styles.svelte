20de2e587ff2035ca028afcee902aa3e8e073862
aliasksandr.zhebit <aliasksandr.zhebit@gmail.com>
2022-07-16 14:10:58 +0300

    styling

---
+++ src/components/styles.svelte
@@ -0,0 +1,29 @@
+<h2 class="section-example--title">Styling</h2>
+
+<div class="section-example">
+  <div class="section-example__view">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aspernatur cumque deleniti distinctio dolor esse
+      ipsum libero minima neque quae qui, quidem quod rem sit tenetur totam vero voluptatem.</p>
+  </div>
+
+  <div class="section-example__code">
+      <pre>
+      &lt;p&gt;
+        <span class="indent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aspernatur cumque deleniti distinctio dolor esse ipsum libero minima neque quae qui, quidem quod rem sit tenetur totam vero voluptatem.
+        </span>
+      &lt;/p&gt;
+
+      &lt;style&gt;
+        <span class="indent">p &#123;</span>
+        <span class="indent"><span class="indent">color: red;</span></span>
+        <span class="indent">&#125;</span>
+      &lt;/style&gt;
+    </pre>
+  </div>
+</div>
+
+<style>
+  p {
+    color: red;
+  }
+</style>
